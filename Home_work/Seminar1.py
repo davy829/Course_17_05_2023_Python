97b05e40749fab21f6411516d5cb1acfa245a708
Andrey <79788106355@mail.ru>
2023-05-22 13:43:54 +0300

    задача№4 шоколадка ломается

--- Home_work/Seminar1.py
+++ Home_work/Seminar1.py
@@ -19,21 +19,32 @@
 # print(f"Серго сплел {int(serg)}шт")
 
 # Задача №2
-num = int(input('Введите номер билетика (6 цифр)_'))
-firstNumer = num / 1000
-secondNumber = num % 1000
-summ1 = 0
-summ2 = 0
-for i in range(0, 3):
-    summ1 = summ1 + firstNumer % 10
-    firstNumer = firstNumer / 10
-    summ2 = summ2 + secondNumber % 10
-    secondNumber = secondNumber / 10
-if summ1 == summ2:
-    print(f"Your tiket is Happy {int(summ1)} + {int(summ2)}")
-else:
-    print(f"Your tiket is not Happy {int(summ1)} + {int(summ2)}")
+# num = int(input('Введите номер билетика (6 цифр)_'))
+# firstNumer = num / 1000
+# secondNumber = num % 1000
+# summ1 = 0
+# summ2 = 0
+# for i in range(0, 3):
+#     summ1 = summ1 + firstNumer % 10
+#     firstNumer = firstNumer / 10
+#     summ2 = summ2 + secondNumber % 10
+#     secondNumber = secondNumber / 10
+# if summ1 == summ2:
+#     print(f"Your tiket is Happy {int(summ1)} + {int(summ2)}")
+# else:
+#     print(f"Your tiket is not Happy {int(summ1)} + {int(summ2)}")
 
+# Задача №3
+widthChoklad = int(input('Введите ширину шоколада_'))
+heightChoklad = int(input('Введите длину шоколада_'))
+pieceChokladWidth = int(input('Введите ширину куска шоколада_'))
+pieceChokladHeight = int(input('Введите длину куска шоколада_'))
 
-
-
+if widthChoklad > pieceChokladWidth:  # шир шок болше шир куска > длина равна длине
+    if pieceChokladHeight == heightChoklad:
+        print('так отломать можно в длину !!!')
+else:   # шир куска делиться без остатка на шир шоколада и длина равна длине куска *так ломать можно
+    if pieceChokladWidth % widthChoklad == 0 and heightChoklad == pieceChokladHeight:
+        print("так ломать можно в ширину !!!")
+    else:
+        print('так отломать нельзя вобще никогда')
